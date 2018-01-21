1c133bae6106b3cf060a21c8dfb81f800d9c2ee6
Deepak <deepakdesai19@gmail.com>
2018-01-21 10:14:36 +0530

    mail settings

--- server.js
+++ server.js
@@ -26,18 +26,18 @@ app.get('/',function(req,res){
 app.post('/email',function(req, res){
 
   var transporter = nodemailer.createTransport({
-   host: 'smtp.zoho.com',
+   host: 'mail.amoreentertainment.in',
     port: 465,
     secure: true, 
     //service: 'gmail',
   auth: {
-    user: 'deepakdesai19@picagroexporter.com',
-    pass: '*2468492383@sandvd'
+    user: 'connect@amoreentertainment.in',
+    pass: '!@Paparocks12'
   }
 });
 
 var mailOptions = {
-  from: 'deepakdesai19@picagroexporter.com',
+  from: 'connect@amoreentertainment.in',
   to: req.body.email,
   subject: 'You Have Successfully login for The Event',
   html: '<h1></h1><br><h2>Name:"'+req.body.fname+''+req.body.lname+'"</h2><br>Email Id:<b>"'+req.body.email+'"</b><br>Contact No : <a href="tel:'+req.body.phone+'"></a><br>Message :<b>"'+req.body.message+'"<b/>'
